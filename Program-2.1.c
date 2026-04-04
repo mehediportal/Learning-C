dc791ab385835cd3f29fe3b2e057604cc414c5e3
Md. Mehedi Hasan <mehediportal@gmail.com>
2026-04-04 15:22:27 +0600

    03:22PM

---
+++ Program-2.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    a = 50;
+    b = 30;
+    sum = a + b;
+    printf("The sum is: %d", sum);
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main ()
+{
+    printf("Mehedi quit pron and masterbation permanently");
+    return 0;
+}
